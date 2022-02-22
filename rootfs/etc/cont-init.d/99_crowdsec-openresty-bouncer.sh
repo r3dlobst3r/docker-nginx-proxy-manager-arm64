18760d4b0469d123fb16b00ce78b05133287dafb
Brian Munro <brian.alexander.munro@gmail.com>
2022-02-22 16:54:56 +0200

    Fixed more bad file names.

--- rootfs/etc/cont-init.d/99_crowdsec-openresty-bouncer.sh
+++ rootfs/etc/cont-init.d/99_crowdsec-openresty-bouncer.sh
@@ -11,11 +11,11 @@ echo "Deploy Crowdsec Openresty Bouncer manually.."
 if [ -f /config/crowdsec/crowdsec-openresty-bouncer.conf ]; then
     echo "Patch crowdsec-openresty-bouncer.conf .." 
     sed "s/=.*//g" /config/crowdsec/crowdsec-openresty-bouncer.conf > /tmp/crowdsec.conf.raw
-    sed "s/=.*//g" /defaults/crowdsec/config_example.conf > /tmp/config_example.conf.raw
-    if grep -vf /tmp/crowdsec.conf.raw /tmp/config_example.conf.raw ; then
-        grep -vf /tmp/crowdsec.conf.raw /tmp/config_example.conf.raw > /tmp/config_example.newvals
+    sed "s/=.*//g" /defaults/crowdsec/crowdsec-openresty-bouncer.conf > /tmp/config_new.conf.raw
+    if grep -vf /tmp/crowdsec.conf.raw /tmp/config_new.conf.raw ; then
+        grep -vf /tmp/crowdsec.conf.raw /tmp/config_new.conf.raw > /tmp/config_new.newvals
         cp /config/crowdsec/crowdsec-openresty-bouncer.conf /config/crowdsec/crowdsec-openresty-bouncer.conf.bak
-        grep -f /tmp/config_example.newvals /defaults/crowdsec/crowdsec-openresty-bouncer.conf >> /config/crowdsec/crowdsec-openresty-bouncer.conf
+        grep -f /tmp/config_new.newvals /defaults/crowdsec/crowdsec-openresty-bouncer.conf >> /config/crowdsec/crowdsec-openresty-bouncer.conf
     fi
 else
     echo "Deploy new crowdsec-openresty-bouncer.conf .." 
